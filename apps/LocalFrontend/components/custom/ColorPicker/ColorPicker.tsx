0ca180082abe3dd0d8f5d0e15aeea514d7e16cb5
MichalMarchel84 <michal.marchel84@gmail.com>
2024-03-12 14:46:49 +0100

    Added offline state to ColorPicker

--- apps/LocalFrontend/components/custom/ColorPicker/ColorPicker.tsx
+++ apps/LocalFrontend/components/custom/ColorPicker/ColorPicker.tsx
@@ -1,7 +1,8 @@
-import {DeviceModel} from "gate-viewmodel";
+import {DeviceModel, DeviceState} from "gate-viewmodel";
 import {RgbColorPicker} from "react-colorful";
 import {useEffect, useMemo, useState} from "react";
 import styles from './ColorPicker.module.css';
+import useModelValue from "@/components/ReactGateViewModel/hooks/useModelValue";
 
 interface ColorPickerProps{
     deviceModel: DeviceModel
@@ -9,6 +10,7 @@ interface ColorPickerProps{
 
 const ColorPicker = (props: ColorPickerProps) => {
     const {deviceModel} = props;
+    const deviceState = useModelValue(deviceModel.state);
     const redModel = useMemo(() => {
         return deviceModel.gateValues.find((value) => value.gateValue.info === "color:red");
     }, [deviceModel]);
@@ -34,7 +36,12 @@ const ColorPicker = (props: ColorPickerProps) => {
 
     return (
         <div className={styles.colorPickerContainer}>
-            <RgbColorPicker color={color} onChange={setColor}/>
+            {deviceState === DeviceState.up &&
+                <RgbColorPicker color={color} onChange={setColor}/>
+            }
+            {deviceState !== DeviceState.up &&
+                <div>Device offline</div>
+            }
         </div>
     )
 }
